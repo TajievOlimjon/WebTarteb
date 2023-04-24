8318c1e42714286e820126937befde3332dfbff0
Olimjon <tajiev0711@gmail.com>
2023-04-24 20:11:40 +0500

    initial

---
+++ WebApi/Program.cs
@@ -0,0 +1,35 @@
+//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc(
+        name: "v1",
+        info:new OpenApiInfo { Title = "Tarteb.Api", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(config =>config.SwaggerEndpoint(
+        url:"swagger/v1/swagger.json",
+        name:"Tarteb.Api"));
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
