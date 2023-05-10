ab1c4a49a5bb532302e0d0c32fb163a67b333956
Olimjon <tajiev0711@gmail.com>
2023-05-10 12:06:15 +0500

    infra : added build project

---
+++ Tarteb.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,69 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Tarteeb Build Process",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2022,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                  {
+                      Name = "Check out"
+                  },
+
+                  new SetupDotNetTaskV1
+                  {
+                      Name = "Setup .Net",
+
+                      TargetDotNetVersion = new TargetDotNetVersion
+                      {
+                          DotNetVersion = "7.0",
+                          IncludePrerelease = true
+                      }
+                  },
+
+                  new RestoreTask
+                  {
+                      Name = "Restore Packages"
+                  },
+
+                  new DotNetBuildTask
+                  {
+                      Name = "Build Projects"
+                  },
+
+                  new TestTask
+                  {
+                      Name = "Run Tests"
+                  }
+            }
+        }
+    }
+};
+
+var adotnetClient = new ADotNetClient();
+
+adotnetClient.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../.github/workflows/build.yml");
